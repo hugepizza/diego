7d79667dc4c642bd2249012af4231fba5b240f4c
Chuanjian Wang <me@ckeyer.net>
2018-03-19 23:09:05 +0800

    remove etcd & unuse vender

--- ptotos/protos.go
+++
@@ -1,3 +0,0 @@
-//go:generate sh -c "protoc -I/usr/local/include -I. -I$GOPATH/src --grpc-gateway_out=logtostderr=true:. --go_out=plugins=grpc:. *.proto"
-// protoc --go_out=. *.proto
-package protos
