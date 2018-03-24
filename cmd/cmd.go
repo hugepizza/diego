1f6422725a5b353a7b2bec5fd4f891bcf6c9623a
Chuanjian Wang <me@ckeyer.net>
2018-03-24 12:10:36 +0800

    update docs

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -3,13 +3,16 @@ package cmd
 import (
 	"github.com/ckeyer/diego/api"
 	"github.com/ckeyer/logrus"
+	"github.com/gomodule/redigo/redis"
 	"github.com/spf13/cobra"
 )
 
 var (
-	addr    string
-	debug   bool
-	dataDir string
+	addr      string
+	debug     bool
+	dataDir   string
+	redisAddr string
+	redisDB   int
 
 	rootCmd = cobra.Command{
 		Use:   "diego",
@@ -22,13 +25,23 @@ func init() {
 	rootCmd.Flags().StringVarP(&addr, "addr", "s", ":8080", "web server listenning address.")
 	rootCmd.Flags().BoolVarP(&debug, "debug", "D", false, "debug level")
 	rootCmd.Flags().StringVarP(&dataDir, "data-dir", "d", "/data", "data storage directory.")
+	rootCmd.Flags().StringVar(&redisAddr, "redis-addr", "127.0.0.1", "redis address.")
+	rootCmd.Flags().IntVar(&redisDB, "redis-db", 0, "redis db.")
 }
 
 // runServe start http server.
 func runServe(cmd *cobra.Command, args []string) {
-	logrus.Infof("listenning at %s", addr)
-	err := api.Serve(addr)
+	redisOpts := []redis.DialOption{
+		redis.DialDatabase(redisDB),
+	}
+	conn, err := redis.Dial("tcp", redisAddr, redisOpts...)
 	if err != nil {
+		logrus.Error(err)
+		return
+	}
+
+	logrus.Infof("listenning at %s", addr)
+	if err := api.Serve(addr); err != nil {
 		logrus.Error(err)
 	}
 }
--- docs/README.md
+++ docs/README.md
@@ -1,5 +1,35 @@
 # API 文档
 
+### 存储结构
+
+* 第一层 命名空间(或者叫组织机构)
+* 第二层 项目名称
+* 第三层 版本+二进制文件
+
+eq.
+```
+google
+  |- kubernetes
+      |- 文件（文件名，版本，大小，hash）
+      |- 1.6.x
+          |- kubecli.tar.gz
+             # https://example.com/google/kubernetes/kubecli.tar.gz?version=x.y.z
+          |- kubelet.tar.gz
+          |- kube-proxy.tar.gz
+      |- 1.7.x
+          |- '/'.tar.gz
+          |- kubelet.tar.gz
+          |- kube-proxy.tar.gz
+      |- 1.9.x
+          |- kubecli.tar.gz
+          |- kubelet.tar.gz
+          |- kube-proxy.tar.gz
+  |- angularjs
+      |- 1.4
+          |- ng-cli.tar.gz
+      |- 2.0
+
+```
+
+
 ### 项目组
-
-
