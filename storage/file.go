a522b6e56ee99d1d74db082f286d8bb56f7f1101
Chuanjian Wang <me@ckeyer.com>
2018-04-21 12:35:00 +0800

    part

---
+++ storage/file.go
@@ -0,0 +1,17 @@
+package storage
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func LoadFromDir(dir string) error {
+	filepath.Walk(dir, walkFn)
+	return nil
+}
+
+func walkFn(path string, info os.FileInfo, err error) error {
+	if info.IsDir() {
+		return nil
+	}
+}
